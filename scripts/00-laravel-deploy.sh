a57a3cca28256b6dc478f817e2feb310327bd612
junosg <shards552@gmail.com>
2024-06-06 20:02:50 +0800

    updated render dockerfile and start sh file

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 echo "Running composer"
+echo "Installing nvm"
+sudo apt-get update
+sudo apt install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm --version
+nvm install --lts
+
+echo "Build frontend"
+npm install
+npm run build
+
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
