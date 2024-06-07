d7eb9087b437d73531ef325a439a461040173768
junosg <shards552@gmail.com>
2024-06-07 10:28:37 +0800

    updated deploy script

--- app/scripts/00-laravel-deploy.sh
+++ app/scripts/00-laravel-deploy.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+apk update
+apk add nodejs
+apk add npm
+npm install
+npm run build
+
 echo "the PWD is : ${PWD}"
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+composer dump-autoload
 
 echo "Caching config..."
 php artisan config:cache
@@ -14,9 +20,3 @@ echo "Running migrations..."
 php artisan migrate --force
 
 chmod -R 777 ./
-
-apk update
-apk add nodejs
-apk add npm
-npm install
-npm run build
