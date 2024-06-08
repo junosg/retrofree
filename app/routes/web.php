69468384334051060d78ae81f4bd9c3a1216903a
juno <shards552@gmail.com>
2024-06-09 02:20:12 +0800

    updated docker setup for local development

--- app/routes/web.php
+++ app/routes/web.php
@@ -5,13 +5,19 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::middleware('guest')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Auth/Login');
+    });
 });
 
 Route::get('/dashboard', function () {
@@ -19,6 +25,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Dashboard');
+    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
--- readme.md
+++ readme.md
@@ -6,13 +6,16 @@
 
     ````cmd
     cd app
+    ````
 
 2. Build the docker image
 
     ````sh
     docker build --tag 'retrofree' . --no-cache --force-rm
+    ````
 
 3. Run the docker image
 
     ````sh
-    docker run -p 3000:80 'retrofree'
+    docker run -d -it --name retrofee --mount type=bind,source="${pwd}",target=/var/www/html -p 3000:80 'retrofree'
+    ````
